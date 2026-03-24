5ebf2a0bcb0a7132f718bc151509f14325cfdd24
sidd19898 <anganesiddhu@gmail.com>
2026-03-24 19:57:04 +0530

    making less months

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function Gitgraph() {
   const selectLast8Months = (contributions) => {
     const now = new Date();
     const past = new Date();
-    past.setMonth(now.getMonth() - 5);
+    past.setMonth(now.getMonth() - 3);
     past.setDate(past.getDate() - 20);
 
     return contributions.map(day => {
